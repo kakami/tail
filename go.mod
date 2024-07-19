1c0baed1836073595cac2eb4238d3b99a63444de
scythefly <fantasticfarm@hotmail.com>
2024-07-19 15:19:38 +0800

    fsnotify

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/hpcloud/tail
+
+go 1.22.1
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7
+)
+
+require golang.org/x/sys v0.4.0 // indirect
--- watch/inotify.go
+++ watch/inotify.go
@@ -10,126 +10,125 @@ import (
 
 	"github.com/hpcloud/tail/util"
 
-	"gopkg.in/fsnotify/fsnotify.v1"
-	"gopkg.in/tomb.v1"
+	"github.com/fsnotify/fsnotify"
 )
 
 // InotifyFileWatcher uses inotify to monitor file changes.
 type InotifyFileWatcher struct {
-	Filename string
-	Size     int64
+    Filename string
+    Size     int64
 }
 
 func NewInotifyFileWatcher(filename string) *InotifyFileWatcher {
-	fw := &InotifyFileWatcher{filepath.Clean(filename), 0}
-	return fw
+    fw := &InotifyFileWatcher{filepath.Clean(filename), 0}
+    return fw
 }
 
 func (fw *InotifyFileWatcher) BlockUntilExists(t *tomb.Tomb) error {
-	err := WatchCreate(fw.Filename)
-	if err != nil {
-		return err
-	}
-	defer RemoveWatchCreate(fw.Filename)
+    err := WatchCreate(fw.Filename)
+    if err != nil {
+        return err
+    }
+    defer RemoveWatchCreate(fw.Filename)
 
-	// Do a real check now as the file might have been created before
-	// calling `WatchFlags` above.
-	if _, err = os.Stat(fw.Filename); !os.IsNotExist(err) {
-		// file exists, or stat returned an error.
-		return err
-	}
+    // Do a real check now as the file might have been created before
+    // calling `WatchFlags` above.
+    if _, err = os.Stat(fw.Filename); !os.IsNotExist(err) {
+        // file exists, or stat returned an error.
+        return err
+    }
 
-	events := Events(fw.Filename)
+    events := Events(fw.Filename)
 
-	for {
-		select {
-		case evt, ok := <-events:
-			if !ok {
-				return fmt.Errorf("inotify watcher has been closed")
-			}
-			evtName, err := filepath.Abs(evt.Name)
-			if err != nil {
-				return err
-			}
-			fwFilename, err := filepath.Abs(fw.Filename)
-			if err != nil {
-				return err
-			}
-			if evtName == fwFilename {
-				return nil
-			}
-		case <-t.Dying():
-			return tomb.ErrDying
-		}
-	}
-	panic("unreachable")
+    for {
+        select {
+        case evt, ok := <-events:
+            if !ok {
+                return fmt.Errorf("inotify watcher has been closed")
+            }
+            evtName, err := filepath.Abs(evt.Name)
+            if err != nil {
+                return err
+            }
+            fwFilename, err := filepath.Abs(fw.Filename)
+            if err != nil {
+                return err
+            }
+            if evtName == fwFilename {
+                return nil
+            }
+        case <-t.Dying():
+            return tomb.ErrDying
+        }
+    }
+    panic("unreachable")
 }
 
 func (fw *InotifyFileWatcher) ChangeEvents(t *tomb.Tomb, pos int64) (*FileChanges, error) {
-	err := Watch(fw.Filename)
-	if err != nil {
-		return nil, err
-	}
+    err := Watch(fw.Filename)
+    if err != nil {
+        return nil, err
+    }
 
-	changes := NewFileChanges()
-	fw.Size = pos
+    changes := NewFileChanges()
+    fw.Size = pos
 
-	go func() {
+    go func() {
 
-		events := Events(fw.Filename)
+        events := Events(fw.Filename)
 
-		for {
-			prevSize := fw.Size
+        for {
+            prevSize := fw.Size
 
-			var evt fsnotify.Event
-			var ok bool
+            var evt fsnotify.Event
+            var ok bool
 
-			select {
-			case evt, ok = <-events:
-				if !ok {
-					RemoveWatch(fw.Filename)
-					return
-				}
-			case <-t.Dying():
-				RemoveWatch(fw.Filename)
-				return
-			}
+            select {
+            case evt, ok = <-events:
+                if !ok {
+                    RemoveWatch(fw.Filename)
+                    return
+                }
+            case <-t.Dying():
+                RemoveWatch(fw.Filename)
+                return
+            }
 
-			switch {
-			case evt.Op&fsnotify.Remove == fsnotify.Remove:
-				fallthrough
+            switch {
+            case evt.Op&fsnotify.Remove == fsnotify.Remove:
+                fallthrough
 
-			case evt.Op&fsnotify.Rename == fsnotify.Rename:
-				RemoveWatch(fw.Filename)
-				changes.NotifyDeleted()
-				return
+            case evt.Op&fsnotify.Rename == fsnotify.Rename:
+                RemoveWatch(fw.Filename)
+                changes.NotifyDeleted()
+                return
 
-			//With an open fd, unlink(fd) - inotify returns IN_ATTRIB (==fsnotify.Chmod)
-			case evt.Op&fsnotify.Chmod == fsnotify.Chmod:
-				fallthrough
+            //With an open fd, unlink(fd) - inotify returns IN_ATTRIB (==fsnotify.Chmod)
+            case evt.Op&fsnotify.Chmod == fsnotify.Chmod:
+                fallthrough
 
-			case evt.Op&fsnotify.Write == fsnotify.Write:
-				fi, err := os.Stat(fw.Filename)
-				if err != nil {
-					if os.IsNotExist(err) {
-						RemoveWatch(fw.Filename)
-						changes.NotifyDeleted()
-						return
-					}
-					// XXX: report this error back to the user
-					util.Fatal("Failed to stat file %v: %v", fw.Filename, err)
-				}
-				fw.Size = fi.Size()
+            case evt.Op&fsnotify.Write == fsnotify.Write:
+                fi, err := os.Stat(fw.Filename)
+                if err != nil {
+                    if os.IsNotExist(err) {
+                        RemoveWatch(fw.Filename)
+                        changes.NotifyDeleted()
+                        return
+                    }
+                    // XXX: report this error back to the user
+                    util.Fatal("Failed to stat file %v: %v", fw.Filename, err)
+                }
+                fw.Size = fi.Size()
 
-				if prevSize > 0 && prevSize > fw.Size {
-					changes.NotifyTruncated()
-				} else {
-					changes.NotifyModified()
-				}
-				prevSize = fw.Size
-			}
-		}
-	}()
+                if prevSize > 0 && prevSize > fw.Size {
+                    changes.NotifyTruncated()
+                } else {
+                    changes.NotifyModified()
+                }
+                prevSize = fw.Size
+            }
+        }
+    }()
 
-	return changes, nil
+    return changes, nil
 }
